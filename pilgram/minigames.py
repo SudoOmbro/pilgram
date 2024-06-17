21754984df2e01b13ee415337e7ad7f3c6af6693
SudoOmbro <ombroso1000@gmail.com>
2024-06-17 22:37:58 +0200

    minigames progress

--- pilgram/minigames.py
+++ pilgram/minigames.py
@@ -1,8 +1,24 @@
 from abc import ABC
 from typing import Tuple
 
+from pilgram.classes import Player
+from pilgram.globals import ContentMeta
+
+
+class GamblingData:
+
+    def __init__(self, game_name: str):
+        self.min_buy_in: int = ContentMeta.get(f"minigames.{game_name}.min_buy_in")
+        self.max_buy_in: int = ContentMeta.get(f"minigames.{game_name}.max_buy_in")
+        self.payout: float = ContentMeta.get(f"minigames.{game_name}.payout")
+
 
 class PilgramMinigame(ABC):
+    """ abstract minigame class, which provides the basic interface for mini-games """
+    IS_GAMBLING: bool
+
+    def __init__(self, player: Player):
+        self.player = player
 
     def play_turn(self, command: str) -> str:
         """ Play a turn of a minigame """
@@ -14,4 +30,12 @@ class PilgramMinigame(ABC):
 
 
 class HandsMinigame(PilgramMinigame):
-    pass
+    IS_GAMBLING = True
+
+    def __init__(self, player: Player):
+        super().__init__(player)
+        self.gambling_data: GamblingData = GamblingData("hands")
+
+    def play_turn(self, command: str) -> str:
+        # TODO
+        pass
