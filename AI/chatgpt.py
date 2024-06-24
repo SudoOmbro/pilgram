934c7c3af4fc0741357791c71b79341e7361a941
SudoOmbro <ombroso1000@gmail.com>
2024-06-24 18:59:21 +0200

    fixed generator not working for artifacts

--- AI/chatgpt.py
+++ AI/chatgpt.py
@@ -205,7 +205,8 @@ class ChatGPTGenerator(PilgramGenerator):
             new_artifact = Artifact(
                 0,
                 name.replace("*", "").replace("\"", ""),
-                description
+                description,
+                None
             )
             result.append(new_artifact)
         return result
